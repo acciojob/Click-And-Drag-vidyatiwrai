8be3df344e7d18d57c1a2357ae38eab8b066df8a
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-20 13:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const items = document.querySelector('.items');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-items.addEventListener('mousedown', (e) => {
-isDown = true;
-items.classList.add('active');
-startX = e.pageX - items.offsetLeft;
-scrollLeft = items.scrollLeft;
-});
-
-items.addEventListener('mouseleave', () => {
-isDown = false;
-items.classList.remove('active');
-});
-
-items.addEventListener('mouseup', () => {
-isDown = false;
-items.classList.remove('active');
-});
-
-items.addEventListener('mousemove', (e) => {
-if (!isDown) return;
-e.preventDefault();
-const x = e.pageX - items.offsetLeft;
-const walk = (x - startX) * 3;
-items.scrollLeft = scrollLeft - walk;
-});
+  const slider = document.querySelector('.items');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+ 
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+ 
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+ 
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+ 
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;  // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+  });
+ 
