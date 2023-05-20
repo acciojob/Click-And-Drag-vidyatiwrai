b5b3722391b360fb081472a33be784a4cb048b4b
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-20 12:52:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Your code here.
+const items = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+items.addEventListener('mousedown', (e) => {
+isDown = true;
+items.classList.add('active');
+startX = e.pageX - items.offsetLeft;
+scrollLeft = items.scrollLeft;
+});
+
+items.addEventListener('mouseleave', () => {
+isDown = false;
+items.classList.remove('active');
+});
+
+items.addEventListener('mouseup', () => {
+isDown = false;
+items.classList.remove('active');
+});
+
+items.addEventListener('mousemove', (e) => {
+if (!isDown) return;
+e.preventDefault();
+const x = e.pageX - items.offsetLeft;
+const walk = (x - startX) * 3;
+items.scrollLeft = scrollLeft - walk;
+});
